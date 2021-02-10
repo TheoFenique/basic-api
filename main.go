b016f7e7b1327bb562941584fa15ac48755d625d
TheoFenique <theo.fenique@gmail.com>
2021-02-10 15:56:06 +0100

    :bug:Added server

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"basic-api/controllers"
+	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
@@ -11,4 +13,5 @@ func main() {
 
 	r.HandleFunc("/articles", controllers.GetArticles).Methods("GET")
 	r.HandleFunc("/articles", controllers.PostArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
