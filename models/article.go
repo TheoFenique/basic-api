3d1217fea63d6b2754ee6ae02408f0a6ee880116
TheoFenique <theo.fenique@gmail.com>
2021-02-28 19:57:01 +0100

    :ambulance:Hotfix models

--- models/article.go
+++ models/article.go
@@ -2,12 +2,12 @@ package models
 
 // Article represent the article
 type Article struct {
-	ID         int       `json:"id"`
-	Username   string    `json:"username"`
-	Title      string    `json:"title"`
-	Content    string    `json:"content"`
-	CreateDate string    `json:"create_date"`
-	Comments   []Comment `json:"comments"`
-	Likes      []int     `json:"likes"`
-	Dislike    []int     `json:"dislike"`
+	ID        int       `json:"id"`
+	Username  string    `json:"username"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt string    `json:"created_at"`
+	Comments  []Comment `json:"comments"`
+	Likes     []int     `json:"likes"`
+	Dislike   []int     `json:"dislike"`
 }
--- models/comment.go
+++ models/comment.go
@@ -2,12 +2,12 @@ package models
 
 // Comment represents the comment
 type Comment struct {
-	ID         int
-	Username   string `json:"username"`
-	Title      string `json:"title"`
-	Content    string `json:"content"`
-	CreateDate string `json:"create_date"`
-	ArticleID  string `json:"article_id"`
-	Likes      []int  `json:"likes"`
-	Dislike    []int  `json:"dislike"`
+	ID        int
+	Username  string `json:"username"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	CreatedAt string `json:"created_at"`
+	ArticleID string `json:"article_id"`
+	Likes     []int  `json:"likes"`
+	Dislike   []int  `json:"dislike"`
 }
